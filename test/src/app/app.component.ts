173a9434767b01ee7413dee4bdf20c58a2724d23
elicul <elicul@symmetria-consulting.hr>
2020-08-28 21:05:54 +0200

    Dropdown added

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  cities1: SelectItem[];
+  selectedCity1: any;
+
+  ngOnInit(): void {
+    this.cities1 = [
+      { label: 'Select City', value: null },
+      { label: 'New York', value: { id: 1, name: 'New York', code: 'NY' } },
+      { label: 'Rome', value: { id: 2, name: 'Rome', code: 'RM' } },
+      { label: 'London', value: { id: 3, name: 'London', code: 'LDN' } },
+      { label: 'Istanbul', value: { id: 4, name: 'Istanbul', code: 'IST' } },
+      { label: 'Paris', value: { id: 5, name: 'Paris', code: 'PRS' } },
+    ];
+  }
+}
--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { AutocompleteDropdownComponent } from './containers/autocomplete-dropdown/autocomplete-dropdown.component';
+import { AutocompleteDropdownModule } from './autocomplete-dropdown/autocomplete-dropdown';
 
 @NgModule({
-  declarations: [AppComponent, AutocompleteDropdownComponent],
-  imports: [BrowserModule, CheckboxModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AutocompleteDropdownModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [AutocompleteDropdownComponent],
+  exports: [],
 })
 export class AppModule {}
