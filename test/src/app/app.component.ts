c8e67e0f6d44627efc397975dd2f994024e07ea1
elicul <elicul@symmetria-consulting.hr>
2020-08-30 20:15:03 +0200

    Update

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { SelectItem } from 'primeng/api';
 import { Dropdown } from 'primeng/dropdown';
 import { AppService } from './app.service';
-import { Observable, fromEvent, Subscribable, Subscription } from 'rxjs';
+import { fromEvent, Subscription } from 'rxjs';
+import { map, distinctUntilChanged, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,31 +13,59 @@ import { Observable, fromEvent, Subscribable, Subscription } from 'rxjs';
 export class AppComponent implements OnInit, OnDestroy {
   @ViewChild('pDropdown', { static: false }) pDropdown: Dropdown;
 
-  cities$: Observable<SelectItem[]>;
+  cities: SelectItem[];
+  cities$: Subscription;
   onScroll$: Subscription;
+  onFilter$: Subscription;
+  searchTxt: string = '';
+  currentPage: number = 1;
   selectedCity: any;
 
   constructor(private appService: AppService) {}
 
   ngOnInit(): void {
-    this.cities$ = this.appService.fetchedItems$;
-    this.appService.fetchItems(0, '');
+    this.cities$ = this.appService.fetchedItems$
+      .pipe(
+        map((x) => {
+          if (this.currentPage > 1) {
+            this.cities = [...this.cities, ...x];
+          } else {
+            this.cities = x;
+          }
+        })
+      )
+      .subscribe();
+    this.appService.fetchItems(this.currentPage, this.searchTxt);
   }
 
-  ngAfterViewInit(): void {
-    // this.pDropdown.filterViewChild.nativeElement.target.value
-  }
+  onDropdownShow(): void {
+    if (!this.onFilter$) {
+      this.onFilter$ = fromEvent(
+        this.pDropdown.filterViewChild.nativeElement,
+        'keyup'
+      )
+        .pipe(
+          map((x: any) => x.target.value),
+          debounceTime(500),
+          distinctUntilChanged()
+        )
+        .subscribe((x) => {
+          console.log('Load new data!');
+          if (!x) {
+            this.searchTxt = '';
+            this.fetchNewItems(false);
+          } else {
+            this.searchTxt = x;
+            this.fetchNewItems(false);
+          }
+        });
+    }
 
-  searchCities(event: any) {
-    console.log(event);
-    // this.appService.fetchItems(0, '');
-  }
-
-  onDropdownShow() {
-    if (this.onScroll$) return;
-
-    this.onScroll$ = fromEvent(this.pDropdown.itemsWrapper, 'scroll').subscribe(
-      () => {
+    if (!this.onScroll$) {
+      this.onScroll$ = fromEvent(
+        this.pDropdown.itemsWrapper,
+        'scroll'
+      ).subscribe(() => {
         const scrollTop = this.pDropdown.itemsWrapper.scrollTop + 200;
         const itemsHeight = this.pDropdown.itemsWrapper.firstElementChild[
           'offsetHeight'
@@ -44,13 +73,25 @@ export class AppComponent implements OnInit, OnDestroy {
 
         if (scrollTop == itemsHeight) {
           console.log('Load new data!');
-          this.appService.fetchItems(1, '');
+          this.fetchNewItems(true);
         }
-      }
-    );
+      });
+    }
+  }
+
+  fetchNewItems(fetchNewPage: boolean): void {
+    if (fetchNewPage) {
+      this.currentPage += 1;
+    } else {
+      this.currentPage = 1;
+    }
+
+    this.appService.fetchItems(this.currentPage, this.searchTxt);
   }
 
   ngOnDestroy(): void {
+    if (this.cities$) this.cities$.unsubscribe();
     if (this.onScroll$) this.onScroll$.unsubscribe();
+    if (this.onFilter$) this.onFilter$.unsubscribe();
   }
 }
--- test/src/app/app.service.ts
+++ test/src/app/app.service.ts
@@ -1,32 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { map, tap, filter } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { SelectItem } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
-  private searchTxt = new BehaviorSubject('');
-  searchTxt$ = this.searchTxt.asObservable();
-
   private fetchedItems = new BehaviorSubject<SelectItem[]>([]);
   fetchedItems$ = this.fetchedItems.asObservable();
 
   constructor(private httpClient: HttpClient) {}
 
   fetchItems(pageNumber: number, searchTxt: string): void {
-    this.searchTxt.next(searchTxt);
-
-    pageNumber += 1;
     const stopItem = pageNumber * 25;
     const startItem = stopItem - 25;
 
-    console.log(searchTxt);
+    let cities$ = this.httpClient.get<SelectItem[]>('assets/cities.json');
 
-    this.httpClient
-      .get<SelectItem[]>('assets/cities.json')
+    if (!!searchTxt)
+      cities$ = cities$.pipe(
+        map((x) =>
+          x.filter((y: any) =>
+            y.city.toLowerCase().includes(searchTxt.toLowerCase())
+          )
+        )
+      );
+
+    cities$
       .pipe(
         map((x) => x.slice(startItem, stopItem)),
         map((x: any) => {
